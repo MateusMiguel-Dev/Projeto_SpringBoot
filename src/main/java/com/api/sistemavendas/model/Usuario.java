30de98008f8c0f1af0752ee95558139c10558f73
MateusMiguel-Dev <miguelmateus285@gmail.com>
2023-10-19 19:06:16 -0300

    Adicionando os métodos a classe usuário

--- src/main/java/com/api/sistemavendas/model/Usuario.java
+++ src/main/java/com/api/sistemavendas/model/Usuario.java
@@ -8,6 +8,63 @@ public class Usuario {
     private String telefone;
     private String cpf;
 
+    public Usuario() {
+    }
 
+    public Usuario(String nome, String senha, String email, Long cod, String telefone, String cpf) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.cod = cod;
+        this.telefone = telefone;
+        this.cpf = cpf;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCod() {
+        return cod;
+    }
+
+    public void setCod(Long cod) {
+        this.cod = cod;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
