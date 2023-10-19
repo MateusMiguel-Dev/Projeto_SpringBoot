10ccfb084111b78f19392ff380e763897c4d0d06
MateusMiguel-Dev <miguelmateus285@gmail.com>
2023-10-19 18:58:36 -0300

    Adicionando a classe Usuário

---
+++ src/main/java/com/api/sistemavendas/model/Usuario.java
@@ -0,0 +1,13 @@
+package com.api.sistemavendas.model;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    private String email;
+    private Long cod;
+    private String telefone;
+    private String cpf;
+
+
+
+}
---
+++ src/test/java/com/api/sistemavendas/SistemaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sistemavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
